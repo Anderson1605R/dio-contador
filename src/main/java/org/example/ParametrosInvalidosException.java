d8c0420588d56e932306f821fe62867b44766d31
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-09-04 13:53:10 -0300

    dio-desafio-contador

---
+++ src/main/java/org/example/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ParametrosInvalidosException  extends Exception{
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
